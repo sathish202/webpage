916591694221e92f9fb625feda8f7b66112f7117
sathish202 <sathesh202@gmail.com>
2024-01-02 17:42:15 +0530

    init

---
+++ index.py
@@ -0,0 +1,17 @@
+# an object of WSGI application
+from flask import Flask, render_template
+# Flask constructor
+app = Flask(__name__)
+
+# A decorator used to tell the application
+# which URL is associated function
+
+
+@app.route('/')
+def home():
+   return(render_template("homepage.html"))
+
+
+
+if __name__=='__main__':
+   app.run()
